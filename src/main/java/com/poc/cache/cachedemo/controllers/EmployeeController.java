e0de78fbc29ce9883769f4ec5b8270f653f8878b
Kanwar Pannu <kanwargurparvezsingh@yahoo.co.in>
2020-02-06 17:42:25 -0500

    Working on controller

--- src/main/java/com/poc/cache/cachedemo/controllers/EmployeeController.java
+++ src/main/java/com/poc/cache/cachedemo/controllers/EmployeeController.java
@@ -1,20 +1,29 @@
 package com.poc.cache.cachedemo.controllers;
 
 import com.poc.cache.cachedemo.models.Employee;
+import com.poc.cache.cachedemo.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api/v1")
 public class EmployeeController {
 
-    //create employee stores in h2 database
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     @PostMapping("/employee")
-    public ResponseEntity<String> createEmployee(@RequestBody Employee employee){
-        return null;
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+        Employee savedEmployee = employeeRepository.save(employee);
+        return new ResponseEntity(savedEmployee, HttpStatus.CREATED);
     }
 
 
@@ -22,7 +31,23 @@ public class EmployeeController {
 
     //get all employee
 
-    //update employee
+    @PutMapping("/employee/{id}")
+    public ResponseEntity<Employee> updateEmployee(@RequestParam("id") String id, @RequestBody Employee employee) {
+        Employee savedEmployee = employeeRepository.findById(Long.parseLong(id)).orElse(null);
+//TODO: missing null case
+        savedEmployee.setName(employee.getName());
+        savedEmployee.setRole(employee.getRole());
+        savedEmployee.setPhoneNumber(employee.getPhoneNumber());
 
-    //delete employee
+        Employee returnedEmployee = employeeRepository.save(savedEmployee);
+
+        return new ResponseEntity(returnedEmployee, HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public ResponseEntity<String> deleteEmployee(@RequestParam("id") String id) {
+        employeeRepository.deleteById(Long.parseLong(id));
+        return new ResponseEntity("employee deleted", HttpStatus.OK);
+        //TODO: what if id not present
+    }
 }
