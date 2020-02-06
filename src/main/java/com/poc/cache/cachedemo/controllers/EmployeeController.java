2dcdb6aba5d52f4d7b3d46d2df7d735d1259e176
Kanwar Pannu <kanwargurparvezsingh@yahoo.co.in>
2020-02-06 01:19:01 -0500

    work in progress - for initial setup

--- src/main/java/com/poc/cache/cachedemo/controllers/EmployeeController.java
+++ src/main/java/com/poc/cache/cachedemo/controllers/EmployeeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api/v1/")
+@RequestMapping(path = "/api/v1")
 public class EmployeeController {
 
     //create employee stores in h2 database
--- src/main/java/com/poc/cache/cachedemo/models/Employee.java
+++ src/main/java/com/poc/cache/cachedemo/models/Employee.java
@@ -1,4 +1,26 @@
 package com.poc.cache.cachedemo.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String role;
+    private String phoneNumber;
 }
